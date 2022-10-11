1d1e17dba0cec79fe1ff688f84501cfad8998ca9
riteshsihag <riteshsihag1@gmail.com>
2022-10-11 13:59:47 +0530

    welcome

--- main-folder/src/App.js
+++ main-folder/src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-    hello
+import {Component} from 'react';
+import Button from './Components/Button'
+class App extends Component {
+render(){
+  return(
+    <div className='App'>
+    <h2>Welcome</h2>
+    <p>Thank you! Happy Learning</p>
+    <Button name = "Subscribe"/> 
     </div>
-  );
+  )
+}
 }
 
 export default App;
