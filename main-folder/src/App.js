99ebf327b8d37b2c3f9fa7fbee90f709fa141394
riteshsihag <riteshsihag1@gmail.com>
2022-10-14 18:00:48 +0530

    comment

--- main-folder/src/App.js
+++ main-folder/src/App.js
@@ -1,52 +1,93 @@
 import './App.css';
 import Card from './Component/card';
 import { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+const initialcontent = []
 
 class App extends Component {
   state = ({
-    content: [
-      {
-        id: "1",
-        element: "Complete the below steps to create your development environment",
-      },
-      {
-        id: "2",
-        element: "Complete the below steps to create your development environment",
-      },
-      {
-        id: "3",
-        element: "Complete the below steps to create your development environment",
-      },
-      {
-        id: "4",
-        element: "Complete the below steps to create your development environment",
-      },
-      {
-        id: "5",
-        element: "Complete the below steps to create your development environment",
-      },
-      {
-        id: "6",
-        element: "Complete the below steps to create your development environment",
-      }
-    ]
+    content: initialcontent,
+    naam: "",
+    comment: "",
+    count:0,
+    
   })
-  deleteUser = (id) =>{
-   const{content} = this.state;
-   const filteredList = content.filter((eachItem) =>{
-   return eachItem.id !== id;
-   })
-   this.setState({content : filteredList});
+
+  addItem = () => {
+    const colors = ['#ff0000', '#00ff00', '#0000ff'];
+const  random_color = colors[Math.floor(Math.random() * colors.length)];
+    const { naam, comment } = this.state;
+    if(naam!=="" && comment!==""){
+    const newContent = {
+      id: uuidv4(),
+      naam,
+      comment,
+      isFavorite: false,
+      random_color
+    }
+    this.setState(prevState => {
+      return {
+        content: [...prevState.content, newContent],
+        naam: "",
+        comment: "",
+        count:prevState.count + 1,
+        
+      }
+    })}
+  };
+
+  changeFavorite=(id)=>{
+    this.setState(prevState=>({
+     content: prevState.content.map(item=>{
+       if(id===item.id){
+        return{...item, isFavorite: !item.isFavorite}
+       } return item;
+     })
+    }))
+   
+   }
+
+  submitName = (event) => {
+    this.setState({
+      naam: event.target.value
+    })
+  }
+
+  submitComment = (event) => {
+    this.setState({
+      comment: event.target.value
+    })
+  }
+
+  deleteUser = (id) => {
+    const { content } = this.state;
+    const filteredList = content.filter((eachItem) => {
+      return eachItem.id !== id;
+    })
+    this.setState(prevState=>{ return{content: filteredList , count: prevState.count-1}});
   }
   render() {
-    const {content} = this.state;
+    const { content, naam, comment, count } = this.state;
     return (
       <div className='container'>
-        <h1>Simple Todos</h1>
-        {content.map(item=>{
-        return <Card key={item.id} content={item} deleteUser={this.deleteUser} para={item.element} />
+        <div className='main'>
+        <div className='write'>
+        <h1 className='heading'>Comments</h1>
+        <p>Say something about 4.0 Technologies</p>
+        <form>
+        <input placeholder='Your Name' value={naam} onChange={this.submitName} type="text" required/> <br />
+        <textarea ty placeholder='Your Comment' value={comment} onChange={this.submitComment} rows="10" cols="35" required/>
+        <button onClick={this.addItem}>Add Comments</button></form>
+        </div>
+        <div className='img-container'>
+          <img className='img' src="https://assets.ccbp.in/frontend/react-js/comments-app/comments-img.png"/>
+        </div>
+        </div>
+       <span className='count-container'> <p className='count'>{count}</p> <p className='com'>Comments</p></span>
+        {content.map(item => {
+          return <Card random_color={item.random_color} changeFavorite={this.changeFavorite} key={item.id} content={item} deleteUser={this.deleteUser} naam={item.naam} comment={item.comment} />
         })}
-     
+
       </div>
     );
   }
--- main-folder/src/Component/card.js
+++ main-folder/src/Component/card.js
@@ -1,18 +1,36 @@
 import {Component} from 'react';
 import './card.css';
 class Card extends Component{
+     
     render(){
-        const {content, deleteUser} = this.props;
-        const {id} = content;
-        console.log(content);
+       
+        const {content, deleteUser, changeFavorite , random_color} = this.props;
+        const {id,isFavorite} = content;
        const onDelete = () =>{
             deleteUser(id);
-           
         }
+       const isLiked=()=>{
+            changeFavorite(id);
+        }
+        const btncolor = isFavorite?"like-color":""
+        const check = isFavorite?"https://assets.ccbp.in/frontend/react-js/comments-app/liked-img.png":"https://assets.ccbp.in/frontend/react-js/comments-app/like-img.png"
         return(
-            <div className='main'>
-              <p>{this.props.para}</p>
-              <button  onClick={onDelete}>Delete</button>
+            <div className='main-container'>
+
+                <div className='name'>
+                    <div style={{backgroundColor: random_color}} className='logo'>{this.props.naam.charAt(0)}</div>
+        <div className='head'>
+        <h1>{this.props.naam}</h1>
+              <p>{this.props.comment}</p>
+        </div>     
+              </div>
+              <div className='delete'>
+              <div className='like'>
+              <img onClick={isLiked} src={check}/>
+              <p onClick={isLiked} id={btncolor}>Like</p>
+              </div>
+              <img className='deleteimg' onClick={onDelete} src="https://assets.ccbp.in/frontend/react-js/comments-app/delete-img.png"/>
+              </div>
             </div>
         )
     }
