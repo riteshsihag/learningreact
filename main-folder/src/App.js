1bd0342d34527e6d65a9ca5ea26ee3886081e66e
riteshsihag <riteshsihag1@gmail.com>
2022-10-15 11:55:57 +0530

    memory

--- main-folder/src/App.js
+++ main-folder/src/App.js
@@ -5,91 +5,80 @@ import { v4 as uuidv4 } from 'uuid';
 const initialcontent = []
 
 class App extends Component {
-  state = ({
-    content: initialcontent,
-    naam: "",
-    date: "",
-
+state = {
+  content: initialcontent,
+  id: uuidv4(),
+  title:"",
+  amount:"",
+  selectValue:"Income"
+}
+changeTitle = (event) =>{
+  this.setState({
+    title: event.target.value
   })
+}
+changeAmount = (event) =>{
+  this.setState({
+    amount: event.target.value
+  })
+}
+changeSelectValue = (event) =>{
+  this.setState({
+    selectValue: event.target.value
+  
+  })
+}
+addItem=()=>{
+  const {id,title,amount,selectValue} = this.state
+  const newContent = [{
+    id:uuidv4(),
+    title,
+    amount,
+    selectValue
+  }]
+  this.setState(prevState=>({
+    content:[...prevState.content, newContent],
+    title:"",
+  amount:"",
+  selectValue:"Income"
+  }))
+}
 
-  addItem = () => {
-    const { naam, date } = this.state;
-    if (naam !== "" && date !== "") {
-      const newContent = {
-        id: uuidv4(),
-        naam,
-        date,
-        isFavorite: false,
-      }
-      this.setState(prevState => {
-        return {
-          content: [...prevState.content, newContent],
-          naam: "",
-          date: ""
-        }
-      })
-    }
-  };
 
-  changeFavorite = (id) => {
-    this.setState(prevState => ({
-      content: prevState.content.map(item => {
-        if (id === item.id) {
-          return { ...item, isFavorite: !item.isFavorite }
-        } return item;
-      })
-    }))
-
-  }
-
-  submitName = (event) => {
-    this.setState({
-      naam: event.target.value
-    })
-  }
-
-  submitdate = (event) => {
-    this.setState({
-      date: event.target.value
-    })
-  }
- 
   render() {
-   
-    const { content, naam, date} = this.state;
-   const starredElement = () =>{
-      const star = content.map(item=>{
-        if(item.isFavorite){
-         content.filter(item=>{
-          
-         })
-        }
-      })
-     }
+  const {content,title,amount,selectValue} = this.state;
+  console.log(content)
     return (
-      <div className='container'>
-        <div className='main'>
-          <div className='write'>
-            <h1 className='heading'>Add Appointment</h1>
-            <form>
-              <p>Title</p>
-              <input placeholder='Title' value={naam} onChange={this.submitName} type="text" required /> <br />
-              <p>Date</p>
-              <input value={date} onChange={this.submitdate} className="inputdate" type="date" rows="10" cols="35" required />
-              <button onClick={this.addItem}>Add</button></form>
-          </div>
-          <div className='img-container'>
-            <img className='img' src="https://assets.ccbp.in/frontend/react-js/appointments-app/appointments-img.png" />
-          </div>
+      <div className='money-container'>
+        <div className='balance-container'>
+
         </div>
-        <div className='star'>
-        <h2>Appointments</h2>
-        <p className='starred' onClick={starredElement}>Starred</p>
+        <div className='transaction-container'>
+          <form>
+            <input value={title} onChange={this.changeTitle} placeholder='Title'  type="text" />
+            <input value={amount} onChange={this.changeAmount} placeholder='Amount' type="text" />
+            <select defaultValue={selectValue} onChange={this.changeSelectValue}>
+              <option value="Income">Income</option>
+              <option value="Expenses">Expenses</option>
+            </select>
+            <button type={'button'} onClick={this.addItem}>Add</button>
+          </form> 
         </div>
-        <div className='flex-container'>
-        {content.map(item => {
-          return <Card changeFavorite={this.changeFavorite} key={item.id} content={item} naam={item.naam} date={item.date} />
-        })}</div>
+        <table>
+          <thead>
+          <tr>
+            <th>Title</th>
+            <th>Amount</th>
+            <th>Type</th>
+          </tr>
+          </thead>
+          <tbody>
+          {content.map(item=>{
+            // console.log(item)
+              return <Card key={item.id} title={item.title} select={item.selectValue} amount={item.amount}/>
+          })}
+          </tbody>
+        </table>
 
       </div>
     );
--- main-folder/src/Component/card.js
+++ main-folder/src/Component/card.js
@@ -3,25 +3,14 @@ import './card.css';
 class Card extends Component {
 
     render() {
-
-        const { content, changeFavorite } = this.props;
-        const { id, isFavorite } = content;
-        const isLiked = () => {
-            changeFavorite(id);
-        }
-        const check = isFavorite ? "https://assets.ccbp.in/frontend/react-js/appointments-app/filled-star-img.png" : "https://assets.ccbp.in/frontend/react-js/appointments-app/star-img.png"
+console.log(this.props.title)
         return (
-            <div className='main-container'>
-
-                <div className='name'>
-                    <div className='head'>
-                        <h1>{this.props.naam}</h1>
-                        <img className='like' onClick={isLiked} src={check} />
-                    </div>
-                    <p className='date'>Date: {this.props.date}</p>
-                </div>
-
-            </div>
+            <tr>
+            <td>{this.props.title}</td>
+            <td>{this.props.amount}</td>
+            <td>{this.props.select}</td>
+            <td><img src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png "/></td>
+            </tr>
         )
     }
 }
