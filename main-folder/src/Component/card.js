bb0332b88d87d61dc8bae3f2707e04784c0b0a78
riteshsihag <riteshsihag1@gmail.com>
2022-10-17 10:20:01 +0530

    emoji

--- main-folder/src/Component/card.js
+++ main-folder/src/Component/card.js
@@ -3,7 +3,6 @@ import './card.css';
 class Card extends Component {
 
     render() {
-<<<<<<< HEAD
        const {getrandomEmoji,id} = this.props;
        const changeImg = () =>{
         getrandomEmoji(id)
@@ -12,19 +11,6 @@ class Card extends Component {
            <div className='emoji' >
             <img onClick={changeImg} src={this.props.url}/>
            </div>
-=======
-        const {deleteElement,id,amount,select} = this.props;
-       const onDelete = () =>{
-            deleteElement(id,amount,select);
-        }
-        return (
-            <tr>
-            <td>{this.props.title}</td>
-            <td>{amount}</td>
-            <td>{select}</td>
-            <td><img className='img' onClick={onDelete} src="https://assets.ccbp.in/frontend/react-js/money-manager/delete.png "/></td>
-            </tr>
->>>>>>> b530803c4d277184cf77080f67c80c99406563b7
         )
     }
 }
