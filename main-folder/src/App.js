2df127106ecfc5021c2e139baedd559076eb679e
riteshsihag <riteshsihag1@gmail.com>
2022-10-17 16:00:15 +0530

    stopwatch

--- main-folder/src/App.js
+++ main-folder/src/App.js
@@ -4,71 +4,48 @@ class App extends Component {
 
   state = ({
     isStart: false,
-    timer: 25,
+    timer: 0,
     seconds: 0
   })
 
   countDownTime = () => {
-    const { timer, seconds } = this.state;
-    const timerFinished = seconds === timer * 60;
-    if (timerFinished) {
-      this.setState({ isStart: false, seconds: 0 })
-    }
-    else {
       this.setState(prevState => ({ seconds: prevState.seconds + 1 }))
-    }
   }
 
   clearTimer = () => {
     clearInterval(this.intervalId)
   }
-  toggleStart = () => {
-    const { isStart } = this.state;
 
-    if (isStart) {
-      this.clearTimer()
-    }
-    else {
+  toggleStart = () => {
       this.intervalId = setInterval(() => {
         this.countDownTime()
       }, 1000)
-    }
-    this.setState(prevState => ({
-      isStart: !prevState.isStart
-    }))
+    
+    this.setState({
+      isStart: true
+    })
   }
 
-
+  stop=()=>{
+    this.clearTimer();
+    this.setState({
+      isStart: false
+    })
+  }
 
   resetTimer = () => {
     this.clearTimer();
     this.setState({
       isStart: false,
-      timer: 25,
+      timer: 0,
       seconds: 0
     })
   }
 
-  decrease = () => {
-    const { isStart } = this.state;
-    if (!isStart) {
-      this.setState(prevState => ({
-        timer: prevState.timer - 1
-      }))
-    }
-  }
-  increase = () => {
-    const { isStart } = this.state;
-    if (!isStart) {
-      this.setState(prevState => ({
-        timer: prevState.timer + 1
-      }))
-    }
-  }
-
+  
   displayTimer = () => {
     const { timer, seconds } = this.state;
-    const timeToSecond = timer * 60 - seconds;
+    const timeToSecond = timer * 60 + seconds;
     var min = Math.floor(timeToSecond / 60);
     var sec = Math.floor(timeToSecond % 60);
     if (min < 10) {
@@ -81,37 +58,21 @@ class App extends Component {
   }
 
   render() {
-    const { isStart, timer } = this.state;
     return (
       <>
-        <h1 className='heading'>Digital Timer</h1>
-      <div className='container'>
-      
-      <div className='timer-img'>
-        <div className='display-timer'>
-          <h1 className='time'>{this.displayTimer()}</h1>
-          <p className='time-para'>{isStart ? "Running" : "Paused"}</p>
+      <h1 className='heading'>StopWatch</h1>
+       <div className='stopwatch'>
+        <div className='timer-head'>
+          <img className='timer-img' src="https://assets.ccbp.in/frontend/react-js/stopwatch-timer.png"/>
+          <h3>Timer</h3>
         </div>
-        </div>
-        <div className='timer-settings'>
-          <div className='play-setting'>
-          <span className='start'>
-            <img className='start-img' onClick={this.toggleStart} src={isStart ? "https://assets.ccbp.in/frontend/react-js/pause-icon-img.png" : "https://assets.ccbp.in/frontend/react-js/play-icon-img.png"} />
-            <p>{isStart ? "Pause" : "Start"}</p>
-          </span>
-          <span className='reset'>
-            <img className='reset-img' onClick={this.resetTimer} src="https://assets.ccbp.in/frontend/react-js/reset-icon-img.png" />
-            <p>Reset</p>
-          </span>
-          </div>
-          <p className='limit'>Set Timer limit</p>
-          <div className='setTimer'>
-            <p onClick={this.decrease}>-</p>
-            <span className='set-btn'>{timer}</span>
-            <p onClick={this.increase}>+</p>
-          </div>
-        </div>
-      </div>
+         <h1>{this.displayTimer()}</h1>
+         <div className='timer-setting'>
+         <button className='start' onClick={this.toggleStart}>Start</button>
+         <button className='stop' onClick={this.stop}>Stop</button>
+         <button className='reset' onClick={this.resetTimer}>Reset</button>
+         </div>
+       </div>
       </>
     );
   }
