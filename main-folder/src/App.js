7f94191b47a37e82edc5547406fa083197c7afa0
riteshsihag <riteshsihag1@gmail.com>
2022-10-19 15:53:06 +0530

    crypto

--- main-folder/src/App.js
+++ main-folder/src/App.js
@@ -9,9 +9,7 @@ class App extends Component {
     
     return (
       <BrowserRouter>
-     
       <Switch>
-      
       <Route exact path="/" component={Home}/>
       <Route path='*' component={NotFound}/>
       </Switch>
