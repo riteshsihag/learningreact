6862f7ffba0a9daff24c5ec33939c9cc25e50718
riteshsihag <riteshsihag1@gmail.com>
2022-10-28 18:18:49 +0530

    commit

--- main-folder/src/Components/Login/login.js
+++ main-folder/src/Components/Login/login.js
@@ -1,10 +1,24 @@
 import React from 'react'
 
-export const Login = () => {
-    const onLogin = () =>{
+export const Login = (props) => {
+    const onSubmitLogin=()=>{
+     const {history} = props
+     history.replace('/')
+    }
+    const onLogin = async(event) =>{
+       event.preventDefault()
+       const url = "https://apis.ccbp.in/login"
+       const options = {
+        method: 'POST'
+       }
+       const response = await fetch(url,options)
+       const data = await response.json()
+       if(response.ok === true){
+       onSubmitLogin()
+       }
 
     }
-    
+
   return (
     <div>
         <h1>Please Login</h1>
