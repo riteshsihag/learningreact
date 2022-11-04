c6636358edb861e0a2acfb972abd2ae97a47b57d
riteshsihag <riteshsihag1@gmail.com>
2022-11-04 12:22:01 +0530

    jobby

---
+++ main-folder/src/details.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+
+class Details extends Component {
+  
+    render() {
+        
+        return (
+            <>
+                
+            </>
+        );
+    }
+}
+
+export default Details;
