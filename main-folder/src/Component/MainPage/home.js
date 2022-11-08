2612fbf6308feb3137aaac57fe8ea42c48df83c2
riteshsihag <riteshsihag1@gmail.com>
2022-11-08 12:27:07 +0530

    fdsg

--- main-folder/src/Component/MainPage/home.js
+++ main-folder/src/Component/MainPage/home.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Navbar from '../NavBar/navbar';
 import './home.css'
 class Home extends Component {
@@ -12,7 +13,7 @@ class Home extends Component {
       <div className='home-content'>
       <h1>Clothes That Get YOU Noticed</h1>
       <p>Fashion is part of the daily air and it does not quite help that it changes all the time. Clothes have always been a marker of the era and we are in a revolution. Your fashion makes you been seen and heard that way you are. So, celebrate the seasons new and exciting fashion in your own way.</p>
-      <button>Shop Now</button>
+      <button><Link to='/products'>Shop Now</Link></button>
       </div>
       <div className='home-banner'>
        <img src='https://assets.ccbp.in/frontend/react-js/nxt-trendz-home-img.png'/>
