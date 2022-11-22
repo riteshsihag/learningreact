83acd63d397518d288a6766960709e168daf5602
riteshsihag <riteshsihag1@gmail.com>
2022-11-22 14:17:53 +0530

    mobx

--- main-folder/src/App.tsx
+++ main-folder/src/App.tsx
@@ -2,20 +2,21 @@ import React from 'react';
 import './App.css';
 import Todo from './Components/Todo/todo';
 import {useTranslation, Trans} from 'react-i18next'
+
 function App() {
   const {i18n} = useTranslation()
   const lngs = {
-    en: {nativeName: 'English'},
-    hi: {nativeName: 'Hindi'}
-  }
+    en : {nativeName: 'English'},
+    hi: {nativeName: 'हिन्दी'}}
+  
   return (
     <div className="App">
      <h1><Trans i18nKey={'heading'}>
       TodoList Application
       </Trans></h1> 
      <select onClick={(e)=>i18n.changeLanguage((e.target as HTMLInputElement).value)}>
-      {Object.keys(lngs).map(lng=>{
-        return <option  key={lng} value={lng} >{lng}</option>
+      {Object.keys(lngs).map((eachlng)=>{
+        return <option  key={eachlng} value={eachlng}>{lngs[eachlng as keyof typeof lngs].nativeName}</option>
       })}
         </select>
 
