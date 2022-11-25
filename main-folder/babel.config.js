849bcc6fc3704584b829027ce2c71c37aab2cc08
riteshsihag <riteshsihag1@gmail.com>
2022-11-25 09:46:02 +0530

    node update

--- main-folder/babel.config.js
+++ main-folder/babel.config.js
@@ -1,11 +1,21 @@
+// {
+//     "presets": [
+//         "@babel/preset-env", "@babel/preset-react"
+//     ],
+//         "plugins": [
+//             [
+//                 require(‘@babel/plugin-proposal-decorators’).default,
+//     {
+//         legacy: true
+//     }
+//         ],
+//         ]
+// }
+
 module.exports = {
-    presets: [
-      '@babel/preset-env',
-      '@babel/preset-react',
-      '@babel/preset-flow',
-    ],
-    plugins: [
-      'babel-plugin-styled-components',
-      '@babel/plugin-proposal-class-properties',
-    ]
-  }
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    ["@babel/plugin-proposal-decorators", { legacy: true }],
+    ["@babel/plugin-proposal-class-properties", { loose: true }],
+  ],
+};
